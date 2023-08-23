f8862ba4b1df40d49e4697d06cbb0db763d8f2c6
Frandorr1 <142456182+Frandorr1@users.noreply.github.com>
2023-08-22 21:47:52 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,41 @@
+let variavelTexto = "texto";
+let variavelNumero = 6;
+console.log(typeof variavelTexto);
+console.log(typeof variavelNumero);
+
+//variavel vazia
+//let variavelvazia;
+//console.log(typeof variavelvazia);
+//let variavelNula = null;
+//console.log(variavelNula);
+
+let a = "b";
+console.log(a);
+console.log(typeof a);
+
+a = null;
+console.log(a);
+console.log(typeof a);
+
+
+//exexircicio2
+
+let nome;
+let idade;
+let numero = 6;
+
+nome = prompt("qual o seu nome?");
+idade = prompt("qual a sua idade?");
+console.log(typeof nome);
+console.log(typeof idade);
+
+console.log(idade);
+console.log(numero + 5);
+
+console.log (numero.toString(+ 5));
+
+//conversão numero
+let n = "42"
+console.log(numero(n));
+console.log(typeof n);
+console.log(typeof numero(n));
